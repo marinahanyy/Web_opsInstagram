bb20c2961d3853b1f0a43536588069cd367e075b
Marina Hany <58513520+marinahanyy@users.noreply.github.com>
2021-04-08 16:23:27 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {useState, Component} from 'react';
+import { StyleSheet, Text, View, Image,TextInput,Button, TouchableOpacity} from 'react-native';
+import Todo from './screens/Bucketlist.js';
+
+
+
+export default function App() {
+  return(
+     <Todo/>
+    );}
